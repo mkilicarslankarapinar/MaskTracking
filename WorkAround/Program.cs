b529ba79ce36c440cd80d1823d8f49fc77e961a4
Muhammet Kılıçarslan Karapınar <mkilicarslankarapinar@gmail.com>
2024-02-26 08:09:18 +0300

    Add project files.

---
+++ WorkAround/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Muhammet Kılıçarslan";
+            person1.LastName = "Karapınar";
+            person1.DateOfBirthYear = 2000;
+            person1.NationalIdentity = 12345678910;
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+
+
+        }
+    }
+}
